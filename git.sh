6334ff4221b873690b6447b1ba1c3c8afe6c789a
Jessica Joseph <jessjeanjo@gmail.com>
2020-01-29 06:25:22 -0500

    ⚡️ refactor file contents, ready and version compl.

---
+++ git.sh
@@ -0,0 +1,12 @@
+#!usr/bin/env bash
+
+##### 🥑
+# Install Git
+# A script designed to install the source control tool, Git
+#
+# Copyright JESSICA JEAN JOSEPH © 2020
+# All Rights Reservered.
+##### 🥑
+
+echo "Installing Git"
+brew install git
